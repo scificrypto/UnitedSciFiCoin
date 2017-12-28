c695c5196f018bfd25e56af446ea2304a2a57f8b
scificrypto <realgrandnagus@gmail.com>
2017-12-28 14:13:23 -0500

    Reduce log spam.

--- src/netbase.cpp
+++ src/netbase.cpp
@@ -366,7 +366,7 @@ bool static ConnectSocketDirectly(const CService &addrConnect, SOCKET& hSocketRe
             int nRet = select(hSocket + 1, NULL, &fdset, NULL, &timeout);
             if (nRet == 0)
             {
-                printf("connection timeout\n");
+                // printf("connection timeout\n");
                 CloseSocket(hSocket);
                 return false;
             }
